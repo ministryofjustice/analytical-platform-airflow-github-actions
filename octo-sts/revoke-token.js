6a410db659bc941b3db3bd5fb56f253decb09e6b
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-06-05 21:24:22 +0000

    feat: implement token request and revocation in octo-sts action
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ octo-sts/revoke-token.js
@@ -0,0 +1,19 @@
+const tok = process.env.TOKEN;
+
+(async function main() {
+    try {
+        const res = await fetch('https://api.github.com/installation/token', {
+            method: 'DELETE',
+            headers: { 'Authorization': `Bearer ${tok}` },
+        });
+
+        if (res.status == 204) {
+            console.log('Token was revoked!');
+        } else {
+            console.log(`::error::${res.status} ${res.statusText}`);
+        }
+    } catch (err) {
+        console.log(`::error::${err.stack}`);
+    }
+
+})();
