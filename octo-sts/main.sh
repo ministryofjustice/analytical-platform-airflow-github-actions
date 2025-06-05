44e08ef791b492406ed1ce9fd9f3615da60fbe40
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-06-05 17:25:19 +0000

    Move to script
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ octo-sts/main.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+##########
+# DEBUG
+##########
+
+ls -lrta
+
+##########
+# MAIN
+##########
+
+git clone --depth 1 --branch v1.0.0 https://github.com/octo-sts/action.git /tmp/octo-sts
+
+##########
+# DEBUG
+##########
+
+ls -lrta /tmp/octo-sts
